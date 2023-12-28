4b218bc1c8efdab379560829bda34fd49bd684d8
obsidian <you@example.com>
2023-12-28 20:48:08 +0530

    Created a Binary Search program in Python

---
+++ binSearch.py
@@ -0,0 +1,45 @@
+flag = 0 			# global variable scope
+
+
+def binSearch(arr, key):	# function for performing binary searching
+    global flag
+    start = 0
+    end = len(arr)-1
+    mid = int(start + (end-start)/2)
+    
+    while(start<=end):
+        
+        if(key == arr[mid]):
+            flag = 1		# setting flag
+            return mid+1	# returning the value if found
+        elif(key>arr[mid]):
+            start = mid +1
+        else:
+            end = mid - 1
+            
+        mid = int(start + (end-start)/2)
+        
+def main():			# creating a main function for ease of use and readability.
+ ind = 0
+ i=0
+ marks=[]
+ totalLength = int(input("Enter the number of elements you wish to insert in the list : "))
+ while(i!=totalLength):	# loop for taking user input
+    
+    ind = int(input("Enter element: "))
+    marks.append(ind)
+    i+=1
+    
+ toSearch = int(input("Enter the element that you want to search : "))
+
+ marks.sort()			# sorting the list, for binary search to work [ monotonic function ]
+
+ index = binSearch(marks.copy(), toSearch)	# using copy keyword to copy the list to the binSearch function. Can be directly passed without copy
+
+ if(flag):
+    print(f"Element {toSearch} was found in the list at position {index}!")
+ else:
+    print(f"Element {toSearch} was not found in the list...")
+    
+        
+main()				# calling the main function
